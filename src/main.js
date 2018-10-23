0cf03f3e6f0e264ee48da8293d748628f91359b7
zjhe <852874089@qq.com>
2018-10-23 16:23:27 +0800

    首页banner

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import App from './App'
 import router from './router'
 // 引入fastclick解决手机端点击延迟300毫秒的问题
 import fastClick from 'fastclick'
+// 引入轮播图
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
 // 重置基本样式
 import 'styles/reset.css'
 // 解决边框在不同分辨屏的不同像素大小
@@ -12,10 +15,16 @@ import 'styles/border.css'
 
 import 'styles/iconfont.css'
 
+// 轮播图样式
+import 'swiper/dist/css/swiper.css'
+
 Vue.config.productionTip = false
 
 // 把fastclick绑定到代码body里面
 fastClick.attach(document.body)
+
+// 使用vue-awesome-swiper
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,14 +1,53 @@
 <template>
-    <div>
-      <h2>这是轮播图</h2>
-    </div>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide  v-for="item of swiperList" :key="item.id">
+        <img class="swiper_img" :src="item.imgUrl"  />
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
 </template>
 
 <script>
-export default{
-  name: 'HomeSwiper'
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        // 定义轮播指标
+        pagination: {
+          el: '.swiper-pagination'
+        },
+        // 循环轮播
+        loop: true,
+        // 设置自动轮播到下一张banner
+        autoplay: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/c6/2467595fffc3b302.jpg_750x200_cca13d51.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1810/eb/5cacf42b59e91e02.jpg_750x200_9c0a77b0.jpg'
+      }]
+    }
+  }
 }
 </script>
 <!--添加scoped使样式只作用于当前组件的样式，不能改变其他组件的样式-->
-<style lang="stylus" scoped>
+<style scoped>
+  /*修改组件里面的代码要用符号>>>进行穿透 wrapper里面出现swiper-pagination-bullet-active 就修改样式*/
+  .wrapper >>> .swiper-pagination-bullet-active{
+    background: #ffffff;
+  }
+  .wrapper{
+    width : 100%;
+    height: 0;
+    overflow: hidden;
+    padding-bottom: 26.67%;
+  }
+  .swiper_img{
+    width: 100%;
+  }
 </style>
